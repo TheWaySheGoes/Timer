091552d5435cfaa73ffdcacb296f4d9930dd66ec
lukas <lukas@DESKTOP-PQQ5LDJ>
2018-11-12 06:41:11 +0100

    new repo

---
+++ src/main/Timer.java
@@ -0,0 +1,105 @@
+package main;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Timer {
+	long startTime=(long) 0;
+	long stopTime=(long) 0;
+	
+	public Timer() {
+	}
+
+	/**
+	 * Starts the {@linkplain Timer} object
+	 */
+	public void startTimer() {
+		startTime = 	System.currentTimeMillis();
+	}
+	/**
+	 * Stops the  object
+	 * @throws TimerSetupException if {@linkplain Timer} not started
+	 */
+	public void stopTimer() throws TimerSetupException{
+		if(startTime==0) {
+			throw new TimerSetupException("Timer must be started first");
+		}
+		stopTime= System.currentTimeMillis();
+	}
+	/**
+	 * Shows current time in sec. without stopping the {@linkplain Timer} object
+	 * @return String current time passed
+	 * @throws TimerSetupException if {@linkplain Timer} not started
+	 */
+	public String currentTime() throws TimerSetupException{
+		if(startTime==0) {
+			throw new TimerSetupException("Timer must be started first");
+		}
+	return	("Current Time in sec: "+((System.currentTimeMillis()-startTime))/1000.000);
+	}	
+	/**
+	 * Shows current time in sec. without stopping the {@linkplain Timer} object
+	 * @param id String identifier for this current time
+	 * @return String current time passed 
+	 * @throws TimerSetupException if {@linkplain Timer} not started
+	 */
+	public String currentTime(String id) throws TimerSetupException{
+		if(startTime==0) {
+			throw new TimerSetupException("Timer must be started first");
+		}
+	return	("Current Time "+id.toUpperCase()+" in sec: "+((System.currentTimeMillis()-startTime))/1000.000);
+
+	}
+	
+	/**
+	 * Shows full time passed in sec. from when {@linkplain Timer} was started to when it was stopped
+	 * @return String time passed from when timer was started to when it was stopped
+	 * @throws TimerSetupException
+	 */
+	public String lapsedTime() throws TimerSetupException{
+		if(startTime==0||stopTime==0) {
+			throw new TimerSetupException("Timer must be started and stopped first");
+		}
+		return ("Lapsed Time in sec: "+((this.stopTime-this.startTime)/1000.0000));
+	}
+
+	/**
+	 * Shows full time passed in se.  from when {@linkplain Timer} was started to when ot was stopped 
+	 * @param id String identifier for this lapsed time
+	 * @return String time passed from when timer was started to when it was stopped
+	 * @throws TimerSetupException
+	 */
+	public String lapsedTime(String id) throws TimerSetupException {
+		if(startTime==0||stopTime==0) {
+				throw new TimerSetupException("Timer must be started and stopped first");
+		}
+		return ("Lapsed Time "+id.toUpperCase()+" in sec: "+((this.stopTime-this.startTime)/1000.0000));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		Timer t1 = new Timer();
+		t1.startTimer();
+	
+			Thread.sleep(1555);
+
+
+			System.out.println(t1.currentTime("test one"));
+
+	
+
+			t1.stopTimer();
+	
+				System.out.println(t1.lapsedTime("ss"));
+		
+	}
+	
+	
+}
+
+	
---
+++ src/main/TimerSetupException.java
@@ -0,0 +1,14 @@
+package main;
+import java.util.Date;
+
+public class TimerSetupException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2524499447920979869L;
+	
+	public TimerSetupException(String string) {
+		super(string);
+	}
+	
+}
